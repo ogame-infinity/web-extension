e2c0bb318e76b9125124c1a5704a4526debba859
Alexis Fasquel <alexis.fsq@gmail.com>
2022-07-29 20:12:18 +0200

    removing again the listener Google does not like

--- background.js
+++ background.js
@@ -1,14 +1,13 @@
-chrome.action.onClicked.addListener((() => {
-    chrome.tabs.create({url: "https://lobby.ogame.gameforge.com/"})
-}));
+// chrome.action.onClicked.addListener((() => {
+//     chrome.tabs.create({url: "https://lobby.ogame.gameforge.com/"})
+// }));
 
-
-chrome.runtime.onMessage.addListener((function (request, sender, sendResponse) {
-if (request.type == "notification") {
-                chrome.notifications.create("", request.message);
-                return sendResponse(request.message)
-            }        
-}));
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.type == "notification") {
+    chrome.notifications.create("", request.message);
+    return sendResponse(request.message);
+  }
+});
 
 /*
 chrome.runtime.onStartup.addListener(function () {
