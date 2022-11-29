f3986692a8ef198df307b8f5b563bef15d3caf6c
alexisfasquel <alexis.fsq@gmail.com>
2022-11-29 16:52:35 +0100

    Removing minifying for Firefox upload

--- packaging.sh
+++ packaging.sh
@@ -33,6 +33,12 @@ sed -i "s/12345/$1/g" "$MANIFEST_FILE_NAME"
 zip -qr -X "ogi-chrome.zip" * 
 echo "Packing zip for chrome complete!"
 
+rm $MAIN_JS_FILE_NAME $CONTENT_JS_FILE_NAME $BG_JS_FILE_NAME $CSS_FILE_NAME
+cp ../$MAIN_JS_FILE_NAME ./$MAIN_JS_FILE_NAME
+cp ../$CONTENT_JS_FILE_NAME ./$CONTENT_JS_FILE_NAME
+cp ../$BG_JS_FILE_NAME ./$BG_JS_FILE_NAME
+cp ../$CSS_FILE_NAME ./$CSS_FILE_NAME
+
 # Modifing chrome-extension:// to moz-extension://
 sed -i "s/chrome/moz/g" "$CSS_FILE_NAME"
 zip -qrm -X "ogi-firefox.zip" * -x "ogi-chrome.zip"
