e8560adf11d5e2713e8ed864f3d04f30acf4a072
alexisfasquel <alexis.fsq@gmail.com>
2022-12-06 16:30:05 +0100

    Fix version number issue (firefox)

--- packaging.sh
+++ packaging.sh
@@ -41,6 +41,8 @@ cp ../$BG_JS_FILE_NAME ./$BG_JS_FILE_NAME
 cp ../$CSS_FILE_NAME ./$CSS_FILE_NAME
 cp ../$MANIFEST_FILE_NAME_V2 ./$MANIFEST_FILE_NAME
 
+sed -i "s/12345/$1/g" "$MANIFEST_FILE_NAME"
+
 # Modifing chrome-extension:// to moz-extension://
 sed -i "s/chrome/moz/g" "$CSS_FILE_NAME"
 zip -qrm -X "ogi-firefox.zip" * -x "ogi-chrome.zip"
