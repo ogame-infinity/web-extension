57b22dd8a4e790a4c3d3a3be7825f3091e5c2994
Bishop341-B <Bishop341-B@users.noreply.github.com>
2023-03-15 11:29:03 +0100

    use selfmade commaSeparator assignment for GF discrepancies and standarization to en-US & de-DE locales

--- ogkush.js
+++ ogkush.js
@@ -103,9 +103,15 @@ function getSeparator(locale, separatorType) {
 }
 
 function toFormatedNumber(value, precision = null, units = false) {
+  const commaSeparator = ["en-US", "en-GB", "ro-RO"];
   let locale = document
     .querySelector("#cookiebanner")
     .getAttribute("data-locale");
+  if (commaSeparator.includes(locale)) {
+    locale = "en-US";
+  } else {
+    locale = "de-DE";
+  }
   if (isNaN(value) || value == undefined || value == null) return undefined;
 
   if (units) {
@@ -166,13 +172,8 @@ function toFormatedNumber(value, precision = null, units = false) {
 }
 
 function fromFormatedNumber(value, int = false) {
-  let locale = document
-    .querySelector("#cookiebanner")
-    .getAttribute("data-locale");
-  // let decimalSeparator = getSeparator(locale, "decimal");
-  // let groupSeparator = getSeparator(locale, "group");
-  let decimalSeparator = LocalizationStrings["decimalPoint"];
-  let groupSeparator = LocalizationStrings["thousandSeperator"];
+  const decimalSeparator = LocalizationStrings["decimalPoint"];
+  const groupSeparator = LocalizationStrings["thousandSeperator"];
   let order = 1;
   if (value.includes("T")) {
     order = 1e12;
