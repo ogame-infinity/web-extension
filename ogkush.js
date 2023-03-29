688ee48eecfc86c1d4effa9c5ed1e558f508ad73
Bishop341-B <Bishop341-B@users.noreply.github.com>
2023-03-29 15:12:01 +0200

    remove innerText in createDOM() function helper again XD

--- ogkush.js
+++ ogkush.js
@@ -92,13 +92,12 @@ Element.prototype.html = function (html) {
   this.innerHTML = DOMPurify.sanitize(html);
 };
 
-function createDOM(element, attributes, textContent, innerText) {
+function createDOM(element, attributes, textContent) {
   const e = document.createElement(element);
   for (const key in attributes) {
     e.setAttribute(key, attributes[key]);
   }
   if (textContent) e.textContent = textContent;
-  if (innerText) e.innerText = innerText;
   return e;
 }
 
