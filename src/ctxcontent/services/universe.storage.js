17b5e4daedd32480194d58709c21a0b9b677f1c5
Sergio Flores Genis <sf.cancun@gmail.com>
2024-01-27 01:46:19 -0600

    Adds functionality to manage universe storage, These enhancements contribute to efficient and organised management of universe storage.
    - A new file is introduced that hosts functionality to manage universe storage in Chrome
    - Implements the 'universeStorageOperator' function to save data associated with a universe and key to Chrome storage
    - Implements the 'universeStorageSupplier' function to retrieve data associated with a universe and key from Chrome storage

---
+++ src/ctxcontent/services/universe.storage.js
@@ -0,0 +1,42 @@
+import { fromJSON, toJSON } from "../../util/json.js";
+
+const storageUniverseKeyBuilder = (universe, key) => `${universe}-${key}-information`;
+
+/**
+ * Saves the provided data to Chrome's local storage.
+ * @template V
+ * @param {string} universe - The universe associated with the data.
+ * @param {string} key - The key associated with the data.
+ * @return {(function(V): Promise<V>)|V} - Returns a promise that will be resolved with the provided data after
+ * saving it in storage.
+ */
+export function universeStorageOperator(universe, key) {
+  const universeStorageKey = storageUniverseKeyBuilder(universe, key);
+  return async (data) => {
+    const json = toJSON(data);
+    const plainObject = JSON.parse(json);
+    chrome.storage.local.set({ [universeStorageKey]: plainObject }).then((_) => void 0);
+    return data;
+  };
+}
+
+/**
+ * Retrieves data from Chrome's local storage.
+ * @template V
+ * @param {string} universe - The universe whose data is to be retrieved.
+ * @param {string} key - The key whose data is to be retrieved.
+ * @return {(function(): Promise<V|undefined>)|V} - Returns a promise that will be resolved with the retrieved data
+ * or "undefined" if no data was found.
+ */
+export function universeStorageSupplier(universe, key) {
+  const universeStorageKey = storageUniverseKeyBuilder(universe, key);
+  return async () => {
+    const result = await chrome.storage.local.get(universeStorageKey);
+    if (Object.hasOwn(result, universeStorageKey)) {
+      const plainObjet = result[universeStorageKey];
+      return fromJSON(JSON.stringify(plainObjet));
+    }
+
+    return undefined;
+  };
+}
