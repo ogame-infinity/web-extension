9873a89b6db87dc9af1d3f080db94ee785c3d81a
Guillaume D <guillaume.deloince@aramisauto.com>
2024-03-11 23:20:21 +0100

    feat(observer): implement reusable observer

---
+++ src/util/observer.js
@@ -0,0 +1,21 @@
+import { getLogger } from "./logger.js";
+
+const OGIObserver = function () {
+  const logger = getLogger("OGI - Observer");
+
+  const MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+  return (element, callback, options = { childList: true, subtree: true }) => {
+    if (!element || element.nodeType !== 1) return;
+
+    const observer = new MutationObserver(callback);
+
+    logger.log(" Observer started ", { element });
+
+    observer.observe(element, options);
+
+    return observer;
+  };
+};
+
+export default OGIObserver;
