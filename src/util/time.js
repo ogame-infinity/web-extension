e42374ac3c04500c0c28aeaf47800692784e61d7
Bishop341-B <Bishop341-B@users.noreply.github.com>
2024-04-04 12:13:42 +0200

    ensure code format

--- src/util/time.js
+++ src/util/time.js
@@ -4,18 +4,17 @@
  * @return {number} time in seconds
  */
 export function getTimeFromISOString(string) {
-    const regexString = string.match(/[a-z]+|[^a-z]+/gi);
-    let time = 0;
-    for (let i = 0; i < regexString.length; i++) {
-      let number = Number(regexString[i]);
-      if (!isNaN(number)) {
-        if (regexString[i + 1] === "M") number *= 60;
-        if (regexString[i + 1] === "H") number *= 60 * 60;
-        if (regexString[i + 1] === "DT") number *= 60 * 60 * 24;
-        time += number;
-        i++;
-      }
+  const regexString = string.match(/[a-z]+|[^a-z]+/gi);
+  let time = 0;
+  for (let i = 0; i < regexString.length; i++) {
+    let number = Number(regexString[i]);
+    if (!isNaN(number)) {
+      if (regexString[i + 1] === "M") number *= 60;
+      if (regexString[i + 1] === "H") number *= 60 * 60;
+      if (regexString[i + 1] === "DT") number *= 60 * 60 * 24;
+      time += number;
+      i++;
     }
-    return time;
   }
-  
+  return time;
+}
