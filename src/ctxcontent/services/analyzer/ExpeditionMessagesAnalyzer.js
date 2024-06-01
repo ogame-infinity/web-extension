6fd12fdb7c1f8ccdc547419597f8b5c93e9ff82b
Bishop341-B <Bishop341-B@users.noreply.github.com>
2024-06-01 22:34:11 +0200

    add missing zero padding also for discoveries

--- src/ctxcontent/services/analyzer/ExpeditionMessagesAnalyzer.js
+++ src/ctxcontent/services/analyzer/ExpeditionMessagesAnalyzer.js
@@ -228,10 +228,9 @@ class ExpeditionMessagesAnalyzer {
       if (discoveries && discoveries[msgId]) return;
 
       const newDate = new Date(message.querySelector(".rawMessageData").getAttribute("data-raw-date"));
-      const datePoint = `${newDate.getDate()}.${(newDate.getMonth() + 1).toString().padStart(2, "0")}.${newDate
-        .getFullYear()
+      const datePoint = `${newDate.getDate().toString().padStart(2, "0")}.${(newDate.getMonth() + 1)
         .toString()
-        .slice(2)}`;
+        .padStart(2, "0")}.${newDate.getFullYear().toString().slice(2)}`;
 
       const sums = discoveriesSums[datePoint] || {
         found: [0, 0, 0, 0],
