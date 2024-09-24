b39f2ed10ccd8b018c7385f0773e8ae110430151
Guillaume D <gdeloince.consulting@gmail.com>
2024-09-25 00:23:52 +0200

    fix(arrows): use ascii instead of special chars

--- src/ogkush.js
+++ src/ogkush.js
@@ -14788,7 +14788,7 @@ class OGInfinity {
               });
 
               movementsList.forEach((m) => {
-                const symbolDirection = m.direction === "go" ? "🡒" : "🡐";
+                const symbolDirection = m.direction === "go" ? "\u{02192}" : "\u{02190}";
 
                 const rowType = DOM.createDOM("div");
                 rowType.appendChild(m.img);
