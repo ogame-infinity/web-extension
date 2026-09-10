d957793437d67a315ae68a58e5e834f5ef0f457f
Bishop341-B <Bishop341-B@users.noreply.github.com>
2026-09-10 23:10:02 +0200

    add children chaining capability to createDOM()

--- src/util/dom.js
+++ src/util/dom.js
@@ -1,9 +1,13 @@
-export function createDOM(element, attributes, textContent) {
+export function createDOM(element, attributes, ...children) {
   const e = document.createElement(element);
   for (const key in attributes) {
     e.setAttribute(key, attributes[key]);
   }
-  if (textContent) e.textContent = textContent;
+
+  // children can be any number of elements or text strings, allowing easy chaining
+  if (children.length > 0) {
+    e.append(...children);
+  }
 
   //if element is a select, and doesn't have dropdownInitialized claass, add it => it prevent Ogame restyling it
   if (element === "select" && !e.classList.contains("dropdownInitialized")) {
